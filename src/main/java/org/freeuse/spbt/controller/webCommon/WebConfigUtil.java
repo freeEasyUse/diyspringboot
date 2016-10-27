458a9b3753ccd3ad2004c97b49b0241f4ca2efcc
yekai123 <yekailoveqq@163.com>
2016-10-27 16:51:53 +0800

    # WARNING: head commit changed in the meantime
    diyspringboot
    
    websocket

--- src/main/java/org/freeuse/spbt/controller/webCommon/WebConfigUtil.java
+++ src/main/java/org/freeuse/spbt/controller/webCommon/WebConfigUtil.java
@@ -1,11 +1,16 @@
 package org.freeuse.spbt.controller.webCommon;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 
 @Configuration
 public class WebConfigUtil extends WebMvcConfigurerAdapter {
-	
+	@Bean  
+    public ServerEndpointExporter serverEndpointExporter (){  
+        return new ServerEndpointExporter();  
+    } 
 }
