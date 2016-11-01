c8ae375a85880473e1b9262bf382286e9040761e
yekailoveqq <yekailoveqq@163.com>
2016-11-01 21:43:40 +0800

    http转向https

--- src/main/java/org/freeuse/spbt/controller/mvcconfig/WebConfigUtil.java
+++ src/main/java/org/freeuse/spbt/controller/mvcconfig/WebConfigUtil.java
@@ -1,9 +1,12 @@
 package org.freeuse.spbt.controller.mvcconfig;
 
 
-import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
-import org.springframework.boot.context.embedded.Ssl;
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -13,7 +16,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 public class WebConfigUtil extends WebMvcConfigurerAdapter {
 	
 	
-/*	@Bean
+/*
+ *  可以通过配置文件进行配置
+ * 	@Bean
 	public EmbeddedServletContainerCustomizer containerCustomizer() {
 	    return new EmbeddedServletContainerCustomizer() {
 	        @Override
@@ -26,5 +31,39 @@ public class WebConfigUtil extends WebMvcConfigurerAdapter {
 	        }
 	    };
 	}*/
+	
+	
+	
+	/**
+	 * 配置http 转向https
+	 */
+	@Bean
+	public EmbeddedServletContainerFactory servletContainer(){
+		TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory(){
+			@Override
+			protected void postProcessContext(Context context){
+				SecurityConstraint securityConstraint = new SecurityConstraint();
+				securityConstraint.setUserConstraint("CONFIDENTIAL");
+				SecurityCollection collection = new SecurityCollection();
+				collection.addPattern("/*");
+				securityConstraint.addCollection(collection);
+				context.addConstraint(securityConstraint);
+			}
+		};
+		tomcat.addAdditionalTomcatConnectors(httpCOnnector());
+		return tomcat;
+	}
+	
+	@Bean
+	public Connector httpCOnnector(){
+		Connector connector =  new Connector("org.apache.coyote.http11.Http11Nio2Protocol");
+		connector.setScheme("http");
+		connector.setPort(8080);
+		connector.setSecure(false);
+		connector.setRedirectPort(8443);
+		return connector;
+	}
+	
+	
 	
 }
