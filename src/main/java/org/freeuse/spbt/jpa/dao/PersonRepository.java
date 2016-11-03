87bb952fd7c7fd1c91dee2050dddce118fa82f43
yekailoveqq <yekailoveqq@163.com>
2016-11-03 08:00:51 +0800

    jpa code

---
+++ src/main/java/org/freeuse/spbt/jpa/dao/PersonRepository.java
@@ -0,0 +1,24 @@
+package org.freeuse.spbt.jpa.dao;
+
+
+import java.util.List;
+
+import org.freeuse.spbt.jpa.entity.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+
+public interface PersonRepository extends JpaRepository<Person,Long>{
+
+	List<Person> findByAddress(String name);
+	
+	
+	Person findByNameAndAddress(String name,String address);
+	
+	@Query("select p from Person p where p.name = :name and p.address = :address")
+	Person withNameAndAddressQuery(@Param("name")String name,@Param("address")String address);
+	
+	Person withNameAndAddressNamedQuery(String name,String address);
+	
+}
