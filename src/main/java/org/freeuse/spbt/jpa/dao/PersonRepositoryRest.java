28e2786b231d80a97aef7273bb3359cde89a5e6b
yekailoveqq <yekailoveqq@163.com>
2016-11-15 23:19:42 +0800

    spring data rest 訂製節點

--- src/main/java/org/freeuse/spbt/jpa/dao/PersonRepositoryRest.java
+++ src/main/java/org/freeuse/spbt/jpa/dao/PersonRepositoryRest.java
@@ -5,9 +5,9 @@ import java.util.List;
 import org.freeuse.spbt.jpa.entity.Person;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
-//import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-//@RepositoryRestResource(collectionResourceRel = "people", path = "people")
+@RepositoryRestResource(path="people")
 public interface PersonRepositoryRest extends PagingAndSortingRepository<Person, Long> {
 
 	List<Person> findByName(@Param("name") String name);
