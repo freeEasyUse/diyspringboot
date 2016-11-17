41f17e878cc072f8446ea44d27dfa45fc4f4978d
yekailoveqq <yekailoveqq@163.com>
2016-11-17 23:48:18 +0800

    修改mongo jpa

--- src/main/java/org/freeuse/spbt/dao/mongo/CityMongoRepository.java
+++ src/main/java/org/freeuse/spbt/dao/mongo/CityMongoRepository.java
@@ -3,9 +3,9 @@ package org.freeuse.spbt.dao.mongo;
 import org.freeuse.spbt.jpa.entity.City;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface CityMongoRepository extends CrudRepository<City, Long>{
+public interface CityMongoRepository extends MongoRepository<City, Long>{
 
 	Page<City> findAll(Pageable pageable);
 
