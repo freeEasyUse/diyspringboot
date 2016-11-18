1553bbf960e1e25e0a088ce5f420cceda2c85e5f
yekailoveqq <yekailoveqq@163.com>
2016-11-18 09:12:32 +0800

    redus

--- src/main/java/org/freeuse/spbt/jpa/dao/redis/RedisTool.java
+++ src/main/java/org/freeuse/spbt/jpa/dao/redis/RedisTool.java
@@ -1,8 +1,11 @@
 package org.freeuse.spbt.jpa.dao.redis;
 
+import javax.annotation.Resource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,8 +15,14 @@ public class RedisTool {
 	private StringRedisTemplate stringRedisTemplate;
 	
 	@Autowired
-	private RedisTemplate<String,Object> redisTemplate;
+	private RedisTemplate<Object,Object> redisTemplate;
 	
+	@Resource(name = "stringRedisTemplate")
+	private ValueOperations<String, String> valOpsStr;
+	
+	
+	@Resource(name = "redisTemplate")
+	private ValueOperations<Object, Object> valOps;
 	
 	public void setValue(String key,Object value){
 	}
